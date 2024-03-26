6c49bc714b72dee4dd950d3c67afb14957e1f3c4
Asousa12 <148540078+Asousa12@users.noreply.github.com>
2024-03-26 11:28:52 +0100

    Delete S04T02N01SousaAndreia directory

--- S04T02N01SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01SousaAndreia/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01SousaAndreia.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message ){
-        super(message);
-    }
-}
